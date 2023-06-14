4f6b41292fbdf569f54e8dc206167cd4bcc3301b
chung-ejy <chung.ejy@gmail.com>
2023-06-14 21:55:18 +0900

    decreased parameters

--- parameters/parameters.py
+++ parameters/parameters.py
@@ -5,15 +5,15 @@ class Parameters(object):
     @classmethod
     def parameters(self):
         values = [True]
-        ceilings = [True,False]
-        classifications = [True,False]
-        market_returns = [1.15,1.5,2]
-        shorts = [True,False]
-        ranks = [True,False]
-        risks = [True,False]
+        ceilings = [True]
+        classifications = [False]
+        market_returns = [1.15]
+        shorts = [False]
+        ranks = [False]
+        risks = [True]
         parameters = []
-        buy_days = [0,1,2,3,4]
-        sell_days = [1,2,3,4,5]
+        buy_days = [1]
+        sell_days = [5]
         for value in values:
             for ceiling in ceilings:
                 for classification in classifications:
