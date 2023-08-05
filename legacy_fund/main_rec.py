bdba44c9c95a6bf7e3b925fd3908b44a71ef0bc5
chung-ejy <chung.ejy@gmail.com>
2023-08-05 05:48:17 -0400

    deleting files

--- legacy_fund/main_rec.py
+++
@@ -1,4 +0,0 @@
-from main_fund import MainFund as mf
-
-fund = mf.load_fund()
-fund.run_recommendation()
--- versions/strats/verified_strats.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class VerifiedStrats(Enum):
-    FINANCIAL = 1
-    SPECULATION = 2
-    EQUITY_ROLLING = 3
-    BITCOIN_SPECULATION = 4
-    BITCOIN_BRUTE = 5
