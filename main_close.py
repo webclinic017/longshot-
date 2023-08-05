261e6122b6c5fba2f91ff9bce788f35f1a03eb30
chung-ejy <chung.ejy@gmail.com>
2023-08-05 06:48:22 -0400

    simplified closing script

--- main_close.py
+++ main_close.py
@@ -1,32 +1,6 @@
 from time import sleep
-# from datetime import datetime, timezone, timedelta
 from alpaca_api.alpaca_api import AlpacaApi
 
-# import pandas as pd
-# import json
-# from main_bab_fund import MainBabFund as mf
-
 alp = AlpacaApi()
-
-# fund = mf.load_fund()
-
-
-# new_york_date = datetime.now(tz=timezone(offset=timedelta(hours=-4)))
-# year = new_york_date.year
-# week = new_york_date.isocalendar()[1]
-
-# for portfolio in fund.portfolios:
-#     try:
-#         pricer_class = portfolio.pricer_class
-#         asset_class = pricer_class.asset_class.value
-#         if asset_class == "stocks":
 closed_orders = alp.live_close_all()
-    #     closed_order_df = pd.DataFrame([json.loads(closed_order.json())["body"] for closed_order in closed_orders])
-    #     portfolio.db.cloud_connect()
-    #     portfolio.db.store("orders",closed_order_df)
-    #     portfolio.db.disconnect()
-    # except Exception as e:
-    #         portfolio.db.cloud_connect()
-    #         portfolio.db.store("errors",pd.DataFrame([{"date":str(datetime.now()),"status":"trade","error":str(e)}]))
-    #         portfolio.db.disconnect()
 sleep(300)
