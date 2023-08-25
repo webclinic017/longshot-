7429e9c2c9c2ff052699c4bf962da84edb6c3214
chung-ejy <chung.ejy@gmail.com>
2023-08-25 09:43:03 -0400

    reporting

--- main_trade_lite.py
+++ main_trade_lite.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from alpaca_api.alpaca_api import AlpacaApi
 from time import sleep
-
+from datetime import datetime
 alp = AlpacaApi()
 
 try:
@@ -11,16 +11,17 @@ try:
 except Exception as e:
     print("close",str(e))
 
-account = alp.live_get_account()
-cash = float(account.cash) * 0.999
-trades = pd.read_csv("trades.csv")
-positions = trades.index.size
 
-for row in trades.iterrows():
-    try:
-        ticker = row[1]["ticker"]
-        amount = float(cash/positions)
-        alp.live_market_order(ticker,amount)
-        print(ticker,amount)
-    except Exception as e:
-        print(ticker,amount,str(e))
+if datetime.now().weekday() < 4:
+    account = alp.live_get_account()
+    cash = float(account.cash) * 0.999
+    trades = pd.read_csv("trades.csv")
+    positions = trades.index.size
+    for row in trades.iterrows():
+        try:
+            ticker = row[1]["ticker"]
+            amount = float(cash/positions)
+            alp.live_market_order(ticker,amount)
+            print(ticker,amount)
+        except Exception as e:
+            print(ticker,amount,str(e))
